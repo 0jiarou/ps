8ec3dc40934d53fa8c575663a77e09523e504126
0jiarou <Apple@AppledeMacBook-Pro-4.local>
2015-10-13 21:22:52 +0800

    over

---
+++ js/jquery.js
@@ -0,0 +1,29 @@
+$('#to_About').click(function(){     
+   
+    $('html,body').animate({scrollTop: '0px'}, 400);    
+
+});   
+      
+$('#to_').click(function(){        
+
+    $('html,body').animate({scrollTop:'1241px'}, 400);    
+
+});    
+
+$('#to_c3').click(function(){        
+
+    $('html,body').animate({scrollTop:'2291px'}, 500);    
+
+});    
+
+$('#to_c4').click(function(){        
+
+    $('html,body').animate({scrollTop:'3710px'}, 500);    
+
+});
+
+$('#to_c5').click(function(){        
+
+    $('html,body').animate({scrollTop:'4695px'}, 500);  
+  
+});
